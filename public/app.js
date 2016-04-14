4586e927773df63063d48bf3c63c59f37ff83e63
dusan vesic <dusan.vesic@gmail.com>
2016-04-15 00:42:25 +0200

    First commit

---
+++ public/app.js
@@ -0,0 +1,51 @@
+angular.module('app', ['ngRoute'])
+  .run(function() {
+    console.log('works');
+  })
+  .config(function($routeProvider, $locationProvider) {
+    
+    $routeProvider
+      .when("/", {
+        controller: "MainController as main",
+        templateUrl: "templates/index.html"
+      })
+      .when("/games/:id", {
+        templateUrl: "templates/game.html",
+        controller: "GameController as game"
+      })
+      .when("/action", {
+        templateUrl: "templates/action.html",
+        controller: "ActionController as action"
+      })
+      .otherwise({redirectTo:'/'});
+  })
+  .controller("MainController", MainController)
+  .controller("GameController", GameController)
+  .controller("ActionController", ActionController);
+  
+function MainController($http) {
+  var main = this;
+  $http.get('games.json')
+    .then(function(res) {
+      main.games = res.data;
+      console.log(res.data);
+    })
+}
+
+function GameController($http, $routeParams) {
+  var game = this;
+  var id = $routeParams.id;
+  game.id = id;
+  $http.get('games.json')
+    .then(function(res) {
+      var games = angular.fromJson(res);
+      console.log(games.data);
+      game.single = _.find(games.data, function(single) {
+        return single.id == $routeParams.id;
+      });
+    })
+}
+
+function ActionController() {
+  var action = this;
+}
